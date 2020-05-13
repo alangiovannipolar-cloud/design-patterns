7a96201716c32a42eab4d3f0d26bc257aaa9358c
Alan Giovanni Polar Corzo <alangiovannipolarcorzo@Alans-MacBook-Pro.local>
2020-05-13 07:03:45 -0500

    [feature/factory] Create knifeMeat and KnifeBread

---
+++ Factory/src/main/java/org/alangiovannipolardev/KnifeMeat.java
@@ -0,0 +1,19 @@
+package org.alangiovannipolardev;
+
+public class KnifeMeat  implements Knife {
+
+    @Override
+    public void sharpen() {
+        System.out.println(KnifeMeat.class + "  sharpen");
+    }
+
+    @Override
+    public void polish() {
+        System.out.println(KnifeMeat.class + "  polish");
+    }
+
+    @Override
+    public void pack() {
+        System.out.println(KnifeMeat.class + "  pack");
+    }
+}
