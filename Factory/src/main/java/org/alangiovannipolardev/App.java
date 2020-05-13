e57052ab4217a2458209c236085c56338d404a64
Alan Giovanni Polar Corzo <alangiovannipolarcorzo@Alans-MacBook-Pro.local>
2020-05-13 07:09:25 -0500

    [feature/factory] Update app file to test the factory

--- Factory/src/main/java/org/alangiovannipolardev/App.java
+++ Factory/src/main/java/org/alangiovannipolardev/App.java
@@ -4,9 +4,13 @@ package org.alangiovannipolardev;
  * Hello world!
  *
  */
-public class App {
-    public static void main(String[] args) {
-
+public class App 
+{
+    public static void main( String[] args )
+    {
+        KnifeStore knifeStore = new KnifeStore(new KnifeFactory());
+        Knife knifeBread = knifeStore.orderKnife("bread");
+        Knife knifeMeat = knifeStore.orderKnife("meat");
     }
 
 }
